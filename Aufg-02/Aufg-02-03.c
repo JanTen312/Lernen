21f7452326acdcb75c688268dbeaa84813171d53
JanTen312 <jantenorth@gmail.com>
2023-01-20 08:25:16 +0100

    Taschenrechner

---
+++ Aufg-02/Aufg-02-03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  int main() {
+  float a,b,c;
+	printf("Geben Sie zahl eins ein: ");
+	scanf("%f",&a);
+	printf("Geben Sie die zweite Zahl ein: ");
+	scanf("%f",&b);
+	c = a + b;
+	printf("Das Ergebnis ist: %f\n",c);
+  return 0;
+  }
+
