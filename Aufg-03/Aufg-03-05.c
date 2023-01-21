076aab442e99986a81e3dc6c6c495af4dc118dbe
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-21 14:26:17 +0100

    Update Aufg-03-05.c
    Kind, Erwachsen, Rentner

--- Aufg-03/Aufg-03-05.c
+++ Aufg-03/Aufg-03-05.c
@@ -2,9 +2,9 @@
   int main() { 
     int a;
     printf("\n\nGeben Sie ihr Alter in Jahren ein: ");
-    scanf("%f",&a);
+    scanf("%d",&a);
     if(a<0) {
-      printf("Sie sind noch nicht geboren!\n\n");
+      printf("Sie sind noch nicht geboren!\n\n");}
       else if(a <=18) printf("Sie sind Jugendlich!\n\n");
       else if(a <=67) printf("Sie sind Erwachsen!\n\n");
       else printf("Sie sind Rentner!\n\n");
