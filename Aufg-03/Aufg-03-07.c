710f277bdf4aae09a6a62d87437f14d51d455b61
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-21 14:55:20 +0100

    Create Aufg-03-07.c

---
+++ Aufg-03/Aufg-03-07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+  int main() {
+    float a,b,c;
+    printf("\n\nGeben Sie Zahl eins ein: ");
+    scanf("%f",&a);
+    printf("Geben Sie Zahl zwei ein: ");
+    scanf("%f",&b);
+    printf("Geben Sie Zahl drei ein: ");
+    scanf("%f",&c);
+    if(a>b>c) {
+      printf("Der Wert eins (%f) ist der Größte\n\n",a);}
+    else if(b>c>a) printf("Der Wert zwei (%f) ist der Größte\n\n",b);
+    else(c>b>a) printf("Der Wert drei (%f) ist der Größte\n\n",c);
+  return 0;
+  }
