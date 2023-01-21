88d3f470618d3280dc7a32fca531723ac30ffbac
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-21 14:56:01 +0100

    Update Aufg-03-07.c

--- Aufg-03/Aufg-03-07.c
+++ Aufg-03/Aufg-03-07.c
@@ -10,6 +10,6 @@
     if(a>b>c) {
       printf("Der Wert eins (%f) ist der Größte\n\n",a);}
     else if(b>c>a) printf("Der Wert zwei (%f) ist der Größte\n\n",b);
-    else(c>b>a) printf("Der Wert drei (%f) ist der Größte\n\n",c);
+    else printf("Der Wert drei (%f) ist der Größte\n\n",c);
   return 0;
   }
