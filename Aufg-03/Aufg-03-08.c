2f4a0a66b549da84dc5601d1702ac9050c0fe2d4
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-23 17:48:03 +0100

    Update Aufg-03-08.c

--- Aufg-03/Aufg-03-08.c
+++ Aufg-03/Aufg-03-08.c
@@ -8,10 +8,10 @@
     printf("Geben Sie die dritte Zahl ein: ");
     scanf("%f",&c);
     if(a>b && a>c) {
-      printf("Die Zahl eins (%.2f) ist am Größten",a);
+      printf("Die Zahl eins (%.2f) ist am Größten\n\n",a);
     }else if(b>a && b>c)
-      printf("Die Zahl zwei (%.2f) ist am Größten",b);
+      printf("Die Zahl zwei (%.2f) ist am Größten\n\n",b);
     else 
-      printf("Die Zahl drei (%.2f) ist am Größten",c);
+      printf("Die Zahl drei (%.2f) ist am Größten\n\n",c);
 return 0;
 }
