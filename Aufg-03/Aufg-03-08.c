0c18ff498d0f6c8715d82ec5288b5c35755123a2
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-23 17:43:54 +0100

    Update Aufg-03-08.c

--- Aufg-03/Aufg-03-08.c
+++ Aufg-03/Aufg-03-08.c
@@ -2,17 +2,17 @@
   int main() {
     double a,b,c;
     printf("Geben Sie die erste Zahl ein: ");
-    scanf("%f",&a);
+    scanf("%d",&a);
     printf("Geben Sie die zweite Zahl ein: ");
-    scanf("%f",&b);
+    scanf("%d",&b);
     printf("Geben Sie die dritte Zahl ein: ");
-    scanf("%f",&c);
+    scanf("%d",&c);
     if(a>b && a>c) {
-      printf("Die Zahl eins (%.2f) ist am Größten",a);
+      printf("Die Zahl eins (%.2d) ist am Größten",a);
     }else if(b>a && b>c) {
-      printf("Die Zahl zwei (%.2f) ist am Größten",b);
+      printf("Die Zahl zwei (%.2d) ist am Größten",b);
     }else 
-      printf("Die Zahl drei (%.2f) ist am Größten",c);
+      printf("Die Zahl drei (%.2d) ist am Größten",c);
   }
 return 0;
 }
