2a3995aa77ea155d854bcaa86d8578e3443522ba
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-24 16:25:45 +0100

    Update Aufg-03-14.c

--- Aufg-03/Aufg-03-14.c
+++ Aufg-03/Aufg-03-14.c
@@ -4,11 +4,12 @@
     a = 1;
     b = 1;
       while(b<=10) {
-      while(a <= 10) {
-        printf("*");
-        a = a+1;
-      }
+        while(a <= 10) {
+          printf("*");
+          a = a+1;
+        }
       printf("\n");
+      b = b+1  
       }
    return 0;
       }  
