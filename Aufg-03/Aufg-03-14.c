f2f1d6b05c4e868591823b13cfe24cd171ae4d38
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-24 16:04:08 +0100

    Create Aufg-03-14.c

---
+++ Aufg-03/Aufg-03-14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+  int main() {
+    int a,b;
+    a = 1;
+    b = 1;
+      for(b=1, b<=10, b++) {
+      while(a <= 10) {
+        print("*");
+        a = a+1,
+      }
+      printf("\n");
+   return 0;
+      }  
+        
