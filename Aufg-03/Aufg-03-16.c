a927d04ff9db762d2204ddf053362ead66bca865
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-24 17:11:25 +0100

    Create Aufg-03-16.c

---
+++ Aufg-03/Aufg-03-16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  int main() {
+    int a,b,c;
+    b=0;
+    printf("\n\nGeben Sie eine Zahl ein die durch 5 Teilbar ist: ");
+    scanf("%d",a,c);
+    while(a != 1) {
+      a = a/5;
+      b = b+1;
+      printf("Die Zahl %d ist %d mal durch 5 Teilbar\n\n",c,b);
+    }
+  return 0;
+  }
