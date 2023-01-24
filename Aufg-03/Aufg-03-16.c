bbf49fe2f5e791ca9b410b869a026b02c0a3a7e2
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-24 17:24:42 +0100

    Update Aufg-03-16.c

--- Aufg-03/Aufg-03-16.c
+++ Aufg-03/Aufg-03-16.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
   int main() {
-    int b,c,a;
+    int b,c;
+    float a;
     b=0;
     printf("\n\nGeben Sie eine Zahl ein die durch 5 Teilbar ist: ");
-    scanf("%d",&a);
+    scanf("%f",&a);
     c=a;
     while(a >= 1) {
       a = a/5;
       b = b+1;
     }
-      printf("Die Zahl %d ist %d mal durch 5 Teilbar\n\n",c,b);
+      printf("Die Zahl %d ist %d mal durch 5 Teilbar der Rest ist %.3f\n\n",c,b,a);
   return 0;
   }
