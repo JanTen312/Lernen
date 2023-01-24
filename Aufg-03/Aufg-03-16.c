c11f8851c730bbe301b61e2246b18625f838339e
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-24 17:21:42 +0100

    Update Aufg-03-16.c

--- Aufg-03/Aufg-03-16.c
+++ Aufg-03/Aufg-03-16.c
@@ -5,7 +5,7 @@
     printf("\n\nGeben Sie eine Zahl ein die durch 5 Teilbar ist: ");
     scanf("%d",&a);
     c=a;
-    while(a != 1) {
+    while(a >= 1) {
       a = a/5;
       b = b+1;
       printf("Die Zahl %d ist %d mal durch 5 Teilbar\n\n",c,b);
