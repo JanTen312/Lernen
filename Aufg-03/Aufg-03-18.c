cd9db7eab496e5acf506ab47d767b4ed6addcdb2
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-24 17:43:55 +0100

    Create Aufg-03-18.c

---
+++ Aufg-03/Aufg-03-18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+  int main() {
+    int a,b;
+    do {
+      while(a<=10)
+      printf("*");
+      a=a+1;
+      do {
+        printf("\n");
+        b=b+1;
+      } while(b<=10);
+    }
+  return 0;
+  }
