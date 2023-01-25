f02c7dfd3442ae6dd64b8fb96574bf6b3f5e323c
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-25 16:57:57 +0100

    Update Aufg-03-23.c

--- Aufg-03/Aufg-03-23.c
+++ Aufg-03/Aufg-03-23.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
   void main() {
   int n, a,eins= 0,zwei= 1,drei;
-  printf("Geben Sie die Schrittweite an: ");
+  printf("\n\nGeben Sie die Dauer an: ");
   scanf("%d",&n);
   for(a=1;a<=n;a++) {
     drei = eins + zwei;
