5b0f790948c702185c293aefcc2f73c8ca850665
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-25 16:10:22 +0100

    Update Aufg-03-21.c

--- Aufg-03/Aufg-03-21.c
+++ Aufg-03/Aufg-03-21.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
   void main() {
     int a,b;
-    for(a=0;a<10;a++) {
-      printf("%d * %d",a,a);
+    printf("\n\n");
+    for(a=1;a<=10;a++) {
+      printf("%d * %d\n",a,a);
     }
+    printf("\n\n");
   }
