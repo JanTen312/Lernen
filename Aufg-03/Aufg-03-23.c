063e3667eba79df24b2c8cc17bb7731907c5a3c6
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-25 17:04:58 +0100

    Update Aufg-03-23.c

--- Aufg-03/Aufg-03-23.c
+++ Aufg-03/Aufg-03-23.c
@@ -4,9 +4,9 @@
   printf("\n\nGeben Sie die Dauer an: ");
   scanf("%d",&n);
   if(n==0) {
-    printf(""\nDer endwert von %d ist: 0\n\n",n);
+    printf("\nDer endwert von %d ist: 0\n\n",n);
     } else if(n==1) {
-      printf(""\nDer endwert von %d ist: 1\n\n",n);
+      printf("\nDer endwert von %d ist: 1\n\n",n);
       }
   for(a=1;a<=n;a++) {
     drei = eins + zwei;
