ec275591349c0dcf95287a062d258722f8eddd24
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-25 17:04:29 +0100

    Update Aufg-03-23.c

--- Aufg-03/Aufg-03-23.c
+++ Aufg-03/Aufg-03-23.c
@@ -3,11 +3,16 @@
   int n, a,eins= 0,zwei= 1,drei;
   printf("\n\nGeben Sie die Dauer an: ");
   scanf("%d",&n);
+  if(n==0) {
+    printf(""\nDer endwert von %d ist: 0\n\n",n);
+    } else if(n==1) {
+      printf(""\nDer endwert von %d ist: 1\n\n",n);
+      }
   for(a=1;a<=n;a++) {
     drei = eins + zwei;
     printf("Der %d Fibu Wert ist: %d\n",a,drei);
     eins = zwei;
     zwei = drei;
   }
-  printf("Der endwert von %d ist: %d\n\n",n,drei);
+  printf("\nDer endwert von %d ist: %d\n\n",n,drei);
   }
