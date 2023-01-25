32715f368c60d0ce1d10903e66ad096d670c0cee
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-25 16:22:16 +0100

    Create Aufg-03-22.c

---
+++ Aufg-03/Aufg-03-22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  void main() {
+    int n,a,b;
+    b=1
+    printf(Geben Sie n ein:  ");
+    scanf("%d",&n);
+    for(a=0;a<n;a++) {
+      b=b+a;
+    }
+    printf("Die Fakultät von %d ist %d\n\n",n,b);
+  }
