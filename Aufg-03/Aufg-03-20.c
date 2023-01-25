4ec68c5bac1410ce5507bfd7d479887f13ce749b
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-25 15:56:29 +0100

    Create Aufg-03-20.c

---
+++ Aufg-03/Aufg-03-20.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  void main() {
+    int a,b;
+    printf("\n\n");
+    for(a=0;a<10;a++) {
+      for(b=0;b<10;b++) {
+        printf("*");
+      }
+      printf("\n");
+      b=0;
+    }
+    printf("\n\n");
+  }
