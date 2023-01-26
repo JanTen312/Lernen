09fa9b0ce0b8d6af5535797b792142ebb13e05c2
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-26 20:30:29 +0100

    Update and rename Aaa.c to Aufg-04-01.c

---
+++ Aufg-04/Aufg-04-01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+  void eingabe(float zahl1, float zahl2) {
+      printf("Geben Sie Zahl 1 ein:  ");
+      scanf("%f",&zahl1);
+      printf("Geben Sie Zahl 2 ein:  ");
+      scanf("%f",&zahl2);
+    }
+  void multiplikation(float ergebniss) {
+    ergebniss = zahl1 * zahl2;
+    }
+    
+  void main() {
+    eingabe(float zahl1, float zahl2);
+    multiplikation(float ergebniss);
+  }
