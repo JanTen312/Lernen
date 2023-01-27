0ec8428e073762bab76a1bfcf85901d4dd6e32b8
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-27 11:14:15 +0100

    Update prototyp.h

--- Aufg-04/Aufg-04-02/prototyp.h
+++ Aufg-04/Aufg-04-02/prototyp.h
@@ -1,12 +1,1 @@
-#include <stdio.h>
-float vergleich() {
-   float a,b;
-   printf("\n\nGeben Sie zahl A ein: ");
-   scanf("%f",&a);
-   printf("Geben Sie zahl B ein: ");
-   scanf("%f",&b);
-   if(a>b) {
-     printf("\nDie größere Zahl ist: %f \n\n",a);
-   }else{
-     printf("\nDie größere Zahl ist: %f \n\n",b);}
- }
+float vergleich(float,float);
