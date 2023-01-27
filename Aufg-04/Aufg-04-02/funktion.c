e5d48018d8fade460f1beb2f0b6569a0aad9cbb4
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-27 11:18:49 +0100

    Create funktion.c

---
+++ Aufg-04/Aufg-04-02/funktion.c
@@ -0,0 +1,11 @@
+void vergleich () {
+  flaot a,b; 
+  printf("\n\nGeben Sie zahl A ein: ");
+   scanf("%f",&a);
+   printf("Geben Sie zahl B ein: ");
+   scanf("%f",&b);
+   if(a>b) {
+     printf("\nDie größere Zahl ist: %f \n\n",a);
+   }else{
+     printf("\nDie größere Zahl ist: %f \n\n",b);}
+}
