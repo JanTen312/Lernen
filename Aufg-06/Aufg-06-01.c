019757753500b6716970ec0d974ab698d89b2a60
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-27 17:42:44 +0100

    Update Aufg-06-01.c

--- Aufg-06/Aufg-06-01.c
+++ Aufg-06/Aufg-06-01.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-
+  void main() {
+  int Note[9];
+  for(int i=1;i<=10;i++) {
+    printf("Geben Sie die %d Note ein: ",i);
+    scanf("%d",Note[i]);
+  }
+  for(int i=1;i<=10;i++) {
+    printf("Geben Sie die %d Note ein: ",Note[i]);
+  }
+  }
