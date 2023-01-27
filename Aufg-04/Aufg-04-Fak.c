d7c907b428d6e7982a0ba4dae47f6ae15c0bf266
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-27 13:34:44 +0100

    Create Aufg-04-Fak.c

---
+++ Aufg-04/Aufg-04-Fak.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  int fakultaet(int n) {
+    if(n == 1) return 1;
+    else return fakultaet(n-1)*n;
+  }
+
+void main() {
+  int n;
+  printf("Geben Sie N ein:  ");
+  scanf("%f";&n);
+  printf("Die Fakultät ist: %.2f\n\n",fakultät(n));
+}
