d5c8e78424ab4b39c21a6d0e7c9109a73fad30f4
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-27 10:16:35 +0100

    Update prototyp.h

--- Aufg-04/Aufg-04-02/prototyp.h
+++ Aufg-04/Aufg-04-02/prototyp.h
@@ -1,2 +1,12 @@
 #include <stdio.h>
-
+ void main() {
+   float a,b;
+   printf("\n\nGeben Sie zahl A ein: ");
+   scanf("%f",&a);
+   printf("\n\nGeben Sie zahl A ein: ");
+   scanf("%f",&a);
+   if(a<b) {
+     return a;
+   }else{
+     return b;}
+ }
