9c39c1fb285844454bae261a4eb02004b4d508f1
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-27 14:55:25 +0100

    Create Aufg-04-05.c

---
+++ Aufg-04/Aufg-04-05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  int fibu(int n) {
+    if(n==0) {
+      return 0;
+    }
+    else if(n==1) {
+      return 1;
+    }
+    else {
+      return n=(n-1)+(n-2);
+    }
+    
+    int main() {
+      int f;
+      printf("Geben Sie n ein: ");
+      scanf("%d",&f);
+      printf(Der die Fibunazi zahl ist: %d",fibu(n);
+    }
