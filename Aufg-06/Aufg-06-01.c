a896d23b10c753fc044180f8565a68866dcc2da0
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-28 14:27:58 +0100

    Update Aufg-06-01.c

--- Aufg-06/Aufg-06-01.c
+++ Aufg-06/Aufg-06-01.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
   void main() {
+  int i,n;
+  float schnitt;
+  n=0;
   int Note[9];
   for(int i=0;i<10;i++) {
     printf("Geben Sie die %d Note ein: ",i+1);
     scanf("%d",&Note[i]);
   }
-  printf("\nGeben Sie die %d Note ein: ",Note[0]);
-  printf("\nGeben Sie die %d Note ein: ",Note[1]);
-  printf("\nGeben Sie die %d Note ein: ",Note[2]);
-  printf("\nGeben Sie die %d Note ein: ",Note[3]);
-  printf("\nGeben Sie die %d Note ein: ",Note[4]);
+  for(i=0;i<10;i++) {
+  n=n+Note[i];
+  }
+  schnitt=n/10;
+  printf("\nDer Notenschnitt ist: %.2f\n\n",schnitt);
   }
