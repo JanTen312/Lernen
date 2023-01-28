ff93d523a3037232866c15d392b599aae0667ccb
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-28 18:14:50 +0100

    Update Aufg-06-03.c

--- Aufg-06/Aufg-06-03.c
+++ Aufg-06/Aufg-06-03.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
   void main() {
     int Zahlen[19] = {1,412,65,3,63,8,6,4,32,46,67,54,22,45,32,487,67,5,43}
-    int size;
-    size= 20;
+    int a;
+    a= 20;
     for(int i=0;i<size;i++) {
       for(int x=0;x<size,x++) {
         if(Zahlen[x]>Zahlen[x+1]) {
