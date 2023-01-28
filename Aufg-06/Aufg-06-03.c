facd8b6717f237fbef24ac531d5d034d4c03e1ba
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-28 18:16:54 +0100

    Update Aufg-06-03.c

--- Aufg-06/Aufg-06-03.c
+++ Aufg-06/Aufg-06-03.c
@@ -3,14 +3,14 @@
     int Zahlen[19] = {1,412,65,3,63,8,6,4,32,46,67,54,22,45,32,487,67,5,43};
     int a;
     a= 20;
-    for(int i=0;i<size;i++) {
-      for(int x=0;x<size,x++) {
+    for(int i=0;i<a;i++) {
+      for(int x=0;x<a,x++) {
         if(Zahlen[x]>Zahlen[x+1]) {
           int hilfe = Zahlen[x];
           Zahlen[x] = Zahlen[x+1];
           Zahlen[x] = hilfe;
         }}}
     for(int y=0;y<20;y++) {
-      printf("Die %d Zahl ist &d\n",i+1,Zahlen[i]);
+      printf("Die %d Zahl ist &d\n",y+1,Zahlen[y]);
     }
   }
