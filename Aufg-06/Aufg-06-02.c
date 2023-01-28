3a91420584a9b518c6cf562e8010df5767b27f40
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-28 14:54:24 +0100

    Update Aufg-06-02.c

--- Aufg-06/Aufg-06-02.c
+++ Aufg-06/Aufg-06-02.c
@@ -4,4 +4,10 @@
     long along[100];
     float afloat[100];
     double adouble[100];
+    
+    
+    printf("int Größe: %d Bytes\n",sizeof(int));
+    printf("long Größe: %d Bytes\n",sizeof(long));
+    printf("float Größe: %d Bytes\n",sizeof(float));
+    printf("double Größe: %d Bytes\n",sizeof(double));
   }
