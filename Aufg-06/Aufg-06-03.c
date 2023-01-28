d77f8c9deb4003fe95fd9613a34bedc580f86351
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-28 18:15:27 +0100

    Update Aufg-06-03.c

--- Aufg-06/Aufg-06-03.c
+++ Aufg-06/Aufg-06-03.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
   void main() {
-    int Zahlen[19] = {1,412,65,3,63,8,6,4,32,46,67,54,22,45,32,487,67,5,43}
+    int Zahlen[19] = {1,412,65,3,63,8,6,4,32,46,67,54,22,45,32,487,67,5,43};
     int a;
     a= 20;
     for(int i=0;i<size;i++) {
@@ -10,7 +10,7 @@
           Zahlen[x] = Zahlen[x+1];
           Zahlen[x] = hilfe;
         }}}
-    for(int y=0;i<20;i++) {
+    for(int y=0;y<20;y++) {
       printf("Die %d Zahl ist &d\n",i+1,Zahlen[i]);
     }
   }
