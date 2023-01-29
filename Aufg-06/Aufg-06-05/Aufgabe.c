8bf89b2f9175c4dfbc6001c98e37a992972bbdc9
JanTen312 <jantenorth@gmail.com>
2023-01-29 18:26:29 +0100

    Aufgabe

--- Aufg-06/Aufg-06-05/Aufgabe.c
+++ Aufg-06/Aufg-06-05/Aufgabe.c
@@ -1,2 +1,7 @@
 #include <stdio.h>
-
+void main() {
+	int paare[5][2]={{1,2},{5,6},{4,4},{23482,32492374},{3,67}};
+	for(int a=0;a<5;a++) {
+	  printf("\nDas Zahlenpaar ist: %d | %d \n",paare[a][0],paare[a][1]);
+	}
+}
