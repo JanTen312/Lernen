51fa07ee76d7e9ef8974fb2006860f5a3c176d24
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-29 13:23:49 +0100

    Update Zinsen.c

--- Aufg-Zinsen/Zinsen.c
+++ Aufg-Zinsen/Zinsen.c
@@ -1,1 +1,33 @@
-aaaaaaa
+#include <stdio.h>
+  int main() {
+    int k,n,kn,a,b;
+    float i;
+    printf("Geben Sie die höhe des Kredits ein: ");
+    scanf("%d",&k);
+    printf("Geben Sie die Laufzeit ein: ");
+    scanf("%d",&n);
+    printf("Geben Sie den Zinssatz ein: ");
+    scanf("%f",&i);
+    i=i/100;
+    printf("Wollen Sie Zins (1) oder Zinseszins (2)? ");
+    scanf("%d",&b);
+      switch (b) {
+        case 1: 
+          for (a=1;a<=n;a++) {
+          kn=k*i*a+k;
+          printf("Ergebniss: %d\n",kn);
+          }
+        break;
+        case 2:
+           for (a=1;a<=n;a++) {
+           kn=k*(1+i);
+           printf("Ergebniss: %d\n",kn);
+           k=kn;
+           }
+        break;
+        default:
+          printf("Falsche Eingabe");
+        break;
+      }
+  return 0;
+  }
